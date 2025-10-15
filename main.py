6a086447d0cf94c1db2a694c5f36a7a8b7dbc66a
BLWings <Luqmaanhakim1@outlook.com>
2025-10-15 23:50:55 +0100

    Update

---
+++ main.py
@@ -0,0 +1,5 @@
+import src.extract.data_loader as dloader
+import src.transform
+
+player_data = dloader.load()
+
--- src/extract/data_loader.py
+++ src/extract/data_loader.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 import os
 
 RAW_DATA_FILE = "../../data/raw/raw_player_data.csv"
@@ -7,8 +7,11 @@ dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, RAW_DATA_FILE)
 
 
+def load():
+    player_data = pd.read_csv(filename)
 
-player_data = pandas.read_csv(filename)
+    print(player_data.shape)
+    print(player_data.head(1))
+    
+    return player_data
 
-print(player_data.shape)
-print(player_data.head(1))
